a4e2350fdf987b484d810b2eaf7a1597a61d6be7
vikashvverma <vikash@programminggeek.in>
2015-07-19 03:40:34 +0530

    Add side navigation :point_up:

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,7 +13,9 @@ angular.module('etestApp')
       Auth.logout();
       $location.path('/login');
     };
-
+    vm.toggleSidenav = function(menuId) {
+      $mdSidenav(menuId).toggle();
+    };
     vm.mainMenu = [
       {
         icon: "fa fa-lock fa-2x",
@@ -22,15 +24,43 @@ angular.module('etestApp')
         url: 'main.verbal'
       },
       {
-        icon: "fa fa-lock",
+        icon: "fa fa-lock fa-2x",
         title: "Aptitude",
         tooltip: "Aptitude Test",
         url: 'main.aptitude'
+      },
+      {
+        icon: "fa fa-lock fa-2x",
+        title: "Leaderboard",
+        tooltip: "Leader Board",
+        url: 'main.leaderboard'
+      },
+      {
+        icon: "fa fa-lock fa-2x",
+        title: "Placement",
+        tooltip: "Placement",
+        url: 'main.placement'
+      },
+      {
+        icon: "fa fa-lock fa-2x",
+        title: "Testimonial",
+        tooltip: "Testimonial",
+        url: 'main.testimonial'
       }
     ];
-    vm.toggleSidenav = function(menuId) {
-      $mdSidenav(menuId).toggle();
-    };
+    vm.extraMenu=[{
+      icon: "fa fa-lock fa-2x",
+      title: "About",
+      tooltip: "About Us",
+      url:'main.aboutus'
+    },{
+      icon: "fa fa-lock fa-2x",
+      title: "Contact us",
+      tooltip: "Contact Us",
+      url:'main.contactus'
+    }
+    ];
+
     vm.userMenu = [
       {
         link : '',
@@ -47,6 +77,11 @@ angular.module('etestApp')
         title: 'Messages',
         icon: 'fa fa-lock fa-2x'
       }
+    ];
+    vm.people = [
+      { name: 'Janet Perkins', img: 'assets/images/100-0.jpeg', newMessage: true },
+      { name: 'Mary Johnson', img: 'assets/images/100-1.jpeg', newMessage: false },
+      { name: 'Peter Carlsson', img: 'assets/images/100-2.jpeg', newMessage: false }
     ];
 
 
